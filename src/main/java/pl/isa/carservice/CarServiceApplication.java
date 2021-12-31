c8859693eeea44eaf27d7d0650b64bec74085ace
Maksym Severyn <severinmaxim2015@gmail.com>
2021-12-31 14:25:58 +0100

    Skeleton added

---
+++ src/main/java/pl/isa/carservice/CarServiceApplication.java
@@ -0,0 +1,14 @@
+package pl.isa.carservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarServiceApplication.class, args);
+        System.out.println("Hi");
+    }
+
+}
