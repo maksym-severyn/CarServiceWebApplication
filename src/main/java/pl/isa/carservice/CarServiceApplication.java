a0bb44dd8c5b0d84db474467d381fb697c67f884
Maksym Severyn <severinmaxim2015@gmail.com>
2021-12-31 17:06:35 +0100

    Entity (Car) added

--- src/main/java/pl/isa/carservice/CarServiceApplication.java
+++ src/main/java/pl/isa/carservice/CarServiceApplication.java
@@ -8,7 +8,7 @@ public class CarServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CarServiceApplication.class, args);
-        System.out.println("Hi");
+        System.out.println("READY");
     }
 
 }
---
+++ src/main/java/pl/isa/carservice/entity/Car.java
@@ -0,0 +1,41 @@
+package pl.isa.carservice.entity;
+
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+    private String registrationNumber;
+    private String name;
+    private Integer manufactureYear;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate carAcceptedDate;
+    private boolean isFixed;
+
+    public Car(String registrationNumber, String name, Integer manufactureYear, LocalDate carAcceptedDate) {
+        this.registrationNumber = registrationNumber;
+        this.name = name;
+        this.manufactureYear = manufactureYear;
+        this.carAcceptedDate = carAcceptedDate;
+        this.isFixed = false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return registrationNumber.replaceAll("\\s","").equalsIgnoreCase(car.registrationNumber.replaceAll("\\s",""));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(registrationNumber.toLowerCase().replaceAll("\\s",""));
+    }
+}
